ffb9ad22001002fa750319d7b77cf6a3f3a99050
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 19:29:02 -0500

    sessions controller added

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
+
+    def user_by_token
+      user = get_user_from_token
+      render json: {
+        user: user
+      }
+    end
+
+    private
+
+    def get_user_from_token
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+                               Rails.application.credentials.devise[:jwt_secret_key]).first
+      user_id = jwt_payload['sub']
+      User.find(user_id.to_s)
+    end
+
+    def respond_with(_resource, _opts = {})
+      if current_user
+        render json: {
+          message: 'You are logged in.',
+          user: current_user
+        }, status: :ok
+      else
+        render json: { message: 'Please log in' }, status: :unauthorized
+      end
+    end
+
+    def respond_to_on_destroy
+      log_out_success && return if current_user
+
+      log_out_failure
+    end
+
+    def log_out_success
+      render json: { message: 'You are logged out.' }, status: :ok
+    end
+
+    def log_out_failure
+      render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
+    end
+  end
+end

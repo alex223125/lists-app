b2c51ff1cb62a5d73ba9e45a26c3bdaac35068ac
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 21:21:11 -0500

    migration with index for Where clause finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_22_021445) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_022009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_021445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.integer "user_id"
     t.index ["deleted_at"], name: "index_lists_on_deleted_at"
+    t.index ["is_public"], name: "index_lists_on_is_public"
   end
 
   create_table "users", force: :cascade do |t|

e8d6b88d4ff42c07270e2ae3c3e61ca87598be9f
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 19:31:34 -0500

    rubocop changes: method name corrected

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ module Users
     respond_to :json
 
     def user_by_token
-      user = get_user_from_token
+      user = user_from_token
       render json: {
         user: user
       }
@@ -13,7 +13,7 @@ module Users
 
     private
 
-    def get_user_from_token
+    def user_from_token
       jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
                                Rails.application.credentials.devise[:jwt_secret_key]).first
       user_id = jwt_payload['sub']

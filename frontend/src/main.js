5048f08ea1a5c41559c31994f8841fa84cc848ee
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 20:30:12 -0500

    initialization of vue app configured

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { store } from './store';
+import router from "./router";
+import VueRouter from "vue-router";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "vuetify/dist/vuetify.min.css";
+import vuetify from '@/plugins/vuetify'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store: store,
+  vuetify : vuetify,
+  router: router,
+  render: h => h(App)
+}).$mount("#app");

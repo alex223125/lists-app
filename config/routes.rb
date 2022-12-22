d91f46337a462a04a22927e7f4d113812dc3ad53
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 19:30:36 -0500

    registration and session routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+  devise_scope :user do
+    get '/user-data', to: 'users/sessions#user_by_token'
+  end
 end

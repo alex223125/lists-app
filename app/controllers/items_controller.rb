0790ae04668e9c6567ab10cbccb99f5fc0e52c9d
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 21:10:28 -0500

    Item entity api endpoint updated

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show update destroy ]
+  before_action :set_item, only: %i[show update destroy]
+  before_action :set_list, only: %i[index]
+  before_action :authenticate_user!, except: %i[index]
 
   # GET /items
   def index
-    @items = Item.all
-
-    render json: @items
+    form = Forms::Items::ItemsSearchForm.new(current_user: current_user,
+                                             list: @list)
+    form.submit
+    render json: form.results
   end
 
   # GET /items/1
@@ -39,13 +44,18 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def item_params
-      params.require(:item).permit(:name, :completion_status, :description, :list_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def item_params
+    params.require(:item).permit(:name, :completion_status, :description, :list_id)
+  end
 end

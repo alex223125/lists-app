1607d6206c8cb5122721d20aedcb5e78c79264f9
Alexey Sologub <alexeysologub51@gmail.com>
2022-12-21 21:02:56 -0500

    List entity added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_22_000945) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_020212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_000945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
